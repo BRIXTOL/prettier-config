ade9e697707cc8f7553c7b39868b5c81f74da691
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-09-10 10:18:25 +0200

    initial commit

---
+++ prettier.js
@@ -0,0 +1,30 @@
+module.exports = {
+  overrides: [
+    {
+      files: [
+        '*.json',
+        '*.jsonc',
+        '.liquidrc',
+        '.liquidrc.json'
+      ],
+      options: {
+        parser: 'json-stringify',
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        htmlWhitespaceSensitivity: 'css',
+        insertPragma: false,
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 75,
+        proseWrap: 'preserve',
+        quoteProps: 'as-needed',
+        requirePragma: false,
+        semi: true,
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+        useTabs: false
+      }
+    }
+  ]
+}
---
+++ readme.md
@@ -0,0 +1,33 @@
+## @brixtol/prettier-config
+
+This package includes the shareable Prettier configuration used by [Brixtol Textiles](https://www.brixtoltextiles.com). Prettier is not applied to JavaScript files.
+
+### Install
+
+[pnpm](https://pnpm.js.org/en/cli/install)
+
+```cli
+pnpm i @brixtol/prettier-config --save-dev
+```
+
+### Usage
+
+We extend configuration from within `package.json` files.
+
+```json
+{
+  'extends': [ 'brixtol']
+}
+```
+
+### Related
+
+- [@brixtol/eslint-config-brixtol](https://github.com/brixtol/eslint-config-brixtol)
+
+### License
+
+Licensed under [MIT](#LICENCE)
+
+---
+
+We [♡](https://www.brixtoltextiles.com/discount/4D3V3L0P3RS]) open source!
